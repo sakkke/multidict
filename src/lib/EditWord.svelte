04ff74f853e86714de14577e55953414e1028401
sakkke <w32w64@gmail.com>
2022-07-10 10:11:44 +0900

    :sparkles: add the component to edit a word

---
+++ src/lib/EditWord.svelte
@@ -0,0 +1,61 @@
+<script>
+    import { supabase } from '$lib/supabaseClient'
+
+    let disabled = false
+
+    export let name
+
+    export let value = JSON.stringify({
+        data: {
+            from: {
+                '<LANGUAGE_FROM>': {
+                    to: {
+                        '<LANGUAGE_TO>': '<WORD_AFTER>',
+                    },
+                },
+            },
+        },
+        name: name || '<WORD_BEFORE>',
+    }, null, 2)
+
+    const handleUpdate = async () => {
+        try {
+            disabled = true
+
+            const date = new Date()
+
+            const values = {
+                ...JSON.parse(value),
+                updated_at: date.toISOString(),
+            }
+
+            const { error } = await supabase
+                .from('words')
+                .upsert(values, { onConflict: 'id' })
+
+            if (error) {
+                throw error
+            }
+
+            alert('OK!')
+
+            window.location.reload()
+        } catch (error) {
+            alert(error.message)
+
+            throw error
+        } finally {
+            disabled = false
+        }
+    }
+</script>
+
+<div>
+    <div>Edit Word</div>
+    <div>Data</div>
+    <textarea bind:value></textarea>
+    <div>
+        <button {disabled} on:click={handleUpdate}>Update</button>
+    </div>
+</div>
+
--- src/routes/words/[word].svelte
+++ src/routes/words/[word].svelte
@@ -1,11 +1,12 @@
 <script>
+    import EditWord from '$lib/EditWord.svelte'
     import { page } from '$app/stores'
     import { supabase } from '$lib/supabaseClient'
 
+    const { word } = $page.params
+
     const getWord = async () => {
         try {
-            const { word } = $page.params
-
             const { data, error, status } = await supabase
                 .from('words')
                 .select(`*`)
@@ -38,6 +39,7 @@
         {:else}
             <p>Page not found</p>
         {/if}
+        <EditWord name={word} value={JSON.stringify(result, null, 2)} />
     {:catch error}
         <p>{ error.message }</p>
     {/await}
